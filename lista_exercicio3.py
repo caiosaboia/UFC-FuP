33759dd6d2ebb060e1d96678b0cc24c409217b68
Caio R. Saboia <103676601+caiosaboia@users.noreply.github.com>
2022-04-18 16:26:39 -0300

    Update lista_exercicio3.py

--- lista_exercicio3.py
+++ lista_exercicio3.py
@@ -1,14 +1,26 @@
-numero = input("Escolha um número: ")
-numero = int(numero)
+numero = int(input("Escolha um número: "))
 
-if (numero % 10) == 0:
-    print("divisivel 10")
-    if (numero % 2) == 0:
-        print("divisivel 2")
-    if (numero % 5) == 0:
-        print("divisivel 5")
+if numero % 10 == 0:
+    print("È divisivel por 2\nÉ divisivel por 5\nÉ divisivel por 10")
 else:
-    print("nao é divisivel") 
+    print("Não é divisivel por 10")
+
+    if numero % 2 == 0:
+        print("Divisivel por 2")
+    else:
+        print("Não é divisivel por 2")
+    if numero % 5 == 0:
+        print("É divisivel por 5")
+    else:
+        print("Não é divisivel por 5")
+if numero % 35 == 0:
+    print("É divisivel por 7\nÉ divisivel por 35")
+else:
+    print("Não é divisivel por 35")
+    if numero % 7 == 0:
+        print("É divisivel por 7")
+    else:
+        print("Não é divisivel por 7")
     
 
 
