d1ca81fffbef94eb35b229cee3d7925f3215e609
Caio R. Saboia <103676601+caiosaboia@users.noreply.github.com>
2022-05-03 13:16:34 -0300

    Update Avaliação1.py

--- Avaliação1.py
+++ Avaliação1.py
@@ -1,21 +1,26 @@
 import random
-A = ["o", "a"]
-B = ["confuso", "fascinante", "substancia", "efemero", "insolito", "esplendido", "primordial", "perfeição", "onirico", "inconcebivel", "imperfeito", "categoria"]
+A = ["O", "A"]
+B = ["confuso", "fascinante", "efemero", "insolito", "esplendido", "primordial", "onirico", "inconcebivel", "imperfeito", "perfeição", "categoria", "substancia"]
 C = ["do", "da"]
-D = ["vida", "mundo", "tempo", "realidade", "alma", "intelecto", "universo", "conhecimento", "humanidade", "sociedade", "tecnologia", "verdade"]
+D = ["mundo", "tempo", "intelecto", "universo", "conhecimento", "sociedade", "tecnologia", "verdade", "vida", "realidade", "alma",  "humanidade"]
 E = ["consiste no", "consiste na"]
-F = ["imperfeição", "diversidade", "contraste", "vastidão", "magia", "simplicidade", "hipocrisia", "subjetividade", "imcompreensão", "friovolidade"]
+F = ["contraste", "vastidão", "magia", "simplicidade", "hipocrisia", "subjetividade", "imcompreensão", "friovolidade", "imperfeição", "diversidade",]
+for x in range(0,5):
+    palavra1 = A[random.randint(0,1)]
+    palavra3 = C[random.randint(0,1)]
+    palavra5 = E[random.randint(0,1)]
+    if palavra1 == "O":
+        palavra2 = B[random.randint(0,8)]
+    else:
+        palavra2 = B[random.randint(9,11)]
 
-palavra1 = random.choice(A)
-palavra2 = []
-palavra3 = []
-palavra4 = []
-palavra5 = []
-palavra6 = []
+    if palavra3 == "do":
+        palavra4 = D[random.randint(0,4)]
+    else:
+        palavra4 = D[random.randint(5,11)]
 
-for x in B:
-    if x[-1] == "o" or x[-1] == "e" or x[-1] == "l":
-        palavra2.append(x)
-    if x[-1] == "a" or x == "perfeição":
-        palavra2.append(x)
-print(palavra2)
+    if palavra5 == "consiste na":
+        palavra6 = F[random.randint(1,9)]
+    else:
+        palavra6 = "constraste"
+    print('{} {} {} {} {} {}'.format(palavra1, palavra2, palavra3, palavra4, palavra5, palavra6))
