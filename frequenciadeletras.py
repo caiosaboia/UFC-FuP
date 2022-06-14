d3afacddf8dc99536bf3fe19e0851f40a31ec9fb
Caio R. Saboia <103676601+caiosaboia@users.noreply.github.com>
2022-06-14 09:35:19 -0300

    Update frequenciadeletras.py

--- frequenciadeletras.py
+++ frequenciadeletras.py
@@ -14,6 +14,22 @@ def obtemLetras(texto):
 res = obtemLetras("minha terra tem palmeiras")
 print(res)
 
+#Creio que essa de baixo seja nao recursiva
+
 for e in res:
     freq = res.count(e)
     print(e, freq)
+    
+def obtemFrequencias(texto):
+    D = dict()
+
+    for letra in texto:
+        valor = D.get(letra)
+        if valor == None:
+            D[letra] = 1
+        else:
+            D[letra] = valor +1
+
+    return D
+
+print(obtemFrequencias("minha terra tem palmeiras"))
